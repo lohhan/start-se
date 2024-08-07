e6fbbdd5e52305fbbf62a8b3c654e6cbec4b32b5
Lohhan <lohhang05@gmail.com>
2024-08-07 16:35:00 -0300

    cadastro usuario feito

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadastros')
+    path('cadastro/', views.cadastro, name='cadastro')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render # pega o html e renderiza ele
+from django.shortcuts import render, redirect # pega o html e renderiza ele
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def cadastro(request):
-   return render(request, 'cadastro.html') 
+   if request.method == "GET":
+      return render(request, 'cadastro.html') 
+   elif request.method == "POST":
+      username = request.POST.get("username")
+      senha = request.POST.get("senha")
+      confirmar_senha = request.POST.get("confirmar_senha")
+
+      if senha != confirmar_senha:
+         messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+         return redirect('/usuarios/cadastro')  
+      
+      if len(senha) < 6:
+         messages.add_message(request, constants.ERROR, 'A senha precisa ter ao menos 6 dígitos.')
+         return redirect('/usuarios/cadastro')  
+
+      users = User.objects.filter(username=username)
+      if users.exists():   
+         messages.add_message(request, constants.ERROR, 'Já existe um usuário com este username.')
+         return redirect('/usuarios/cadastro')
+      
+      user = User.objects.create_user(
+         username=username,
+         password=senha
+      )
+
+      return redirect('/usuarios/logar')
